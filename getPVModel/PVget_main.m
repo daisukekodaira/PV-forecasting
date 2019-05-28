1f28c96ad2a557b3f7771b469a24ae97d8ce14c3
daisukekodaira <daisuke.kodaira03@gmail.com>
2019-05-28 14:07:51 +0900

    release for 1.0

--- getPVModel/PVget_main.m
+++
@@ -1,7 +0,0 @@
-clear all;
-clc;
-close all;
-pass = pwd;
-y_pred = PVget_getPVModel([pwd,'\','shortterm.csv'],...
-                        [pwd,'\','forecast.csv'],...
-                        [pwd,'\','ResultData.csv'])
--- setPVModel/PVset_main.m
+++
@@ -1,4 +0,0 @@
-clear all;
-clc;
-close all;
-y_pred = PVset_setPVModel([pwd, '\','testDataSet.csv']);
