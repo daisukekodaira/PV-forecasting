d891b8f197cf92baa83098012c0dd18052556639
daisukekodaira <daisuke.kodaira03@gmail.com>
2019-11-04 15:51:13 +0900

    File path

--- getPVModel/PVget_main.m
+++
@@ -1,7 +0,0 @@
-clear all
-clc
-close all
-warning('off','all');
-y_pred = PVget_getPVModel([pwd,'\','PST_201902082359.csv'],...
-                        [pwd,'\','PFP_201902082359.csv'],...
-                        [pwd,'\','ResultData.csv'])
--- setPVModel_newversion/PVset_main.m
+++
@@ -1,4 +0,0 @@
-clear all;
-clc;
-close all;
-y_pred = PVset_setPVModel([pwd, '\','PLT_201902102359DDG.csv']);
