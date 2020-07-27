d2052453b3131e72f72918d77c8c8724bdbbd220
daisukekodaira <daisuke.kodaira03@gmail.com>
2020-07-27 10:41:44 +0900

    erase

--- getPVModel/PVget_main.m
+++
@@ -1,7 +0,0 @@
-clear all;
-clc;
-close all;
-warning('off','all');
-y_pred = PVget_getPVModel([pwd,'\','PV_ShortTerm_201808010819.csv'],...
-                        [pwd,'\','PV_Forecast_201808010802.csv'],...
-                        [pwd,'\','ResultData.csv'])
--- setPVModel/PVset_main.m
+++
@@ -1,4 +0,0 @@
-clear all;
-clc;
-close all;
-y_pred = PVset_setPVModel([pwd, '\','PV_LongTerm_201807312321.csv']);
