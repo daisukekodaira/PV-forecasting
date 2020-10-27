ad0456028ce2a67d13d44e854ad1df9b5d77d4f9
kazukitukazaki <67730626+kazukitukazaki@users.noreply.github.com>
2020-10-27 18:04:46 +0900

    Add files via upload

--- new code/setPVModel/PVset_pso_main_4.m
+++ new code/setPVModel/PVset_pso_main_4.m
@@ -1,32 +1,30 @@
 % y_ture: True load [MW]
 % y_predict: predicted load [MW]
 function coeff_4 = PVset_pso_main_4(y_predict, y_true)
-    start_pso_main = tic;
-    % Declare the global variables
-    global g_y_true;
-    global g_y_predict;    
+    start_pso_main = tic;    
     % Initialization
     methods = size(y_predict, 2);
     days = size(y_predict(1).data,2);   
+    hours=size(y_true, 1)/days/2;
     % Restructure the predicted data
     for j = 1:methods % the number of prediction methods (k-means and fitnet)
-        for hour = 1:24
+        for hour = 1:hours
             yPredict(hour).data(:,j) = reshape(y_predict(j).data(1+(hour-1)*2:hour*2,:), [],1); % this global variable is utilized in 'objective_func'
         end
     end   
-    % Restructure the target data
-    for day = 1:days
+   % Restructure the target data
+   for day = 1:days
        initial = 1+(day-1)*48;
-       for hour = 1:24    
+       for hour = 1:hours    
            yTarget(hour).data(1+(day-1)*2:2*day,1) = reshape(y_true(initial+(hour-1)*2:initial-1+hour*2,:), [],1); 
        end
-    end
-    % Essential paramerters for PSO performance
-    for hour = 1:24
+   end
+                % Essential paramerters for PSO performance
+    for hour = 1:hours
         g_y_predict = yPredict(hour).data;
         g_y_true = yTarget(hour).data;
         rng default  % For reproducibility
-        nvars = 4;
+        nvars = methods;
         lb = [0,0,0,0];
         ub = [1,1,1,1];
         options = optimoptions('particleswarm', 'MaxIterations',2000,'FunctionTolerance', 1e-25, 'MaxStallIterations', 1500,'Display', 'none');
@@ -34,12 +32,10 @@ function coeff_4 = PVset_pso_main_4(y_predict, y_true)
         [coeff_4(hour, :),~,~,~] = particleswarm(objFunc,nvars,lb,ub, options);   
     end
     function total_err = objectiveFunc(weight, forecast, target) % objective function
-        ensembleForecasted = sum(forecast.*weight, 2);  % Make forecast result based on an ensemble model
-        % err1: [objective]   Deviation between target and forecasted 
-        % err2: [constraint]  The total of the weight which should converge on 1
+        ensembleForecasted = sum(forecast.*weight, 2);  % add two methods
         err1 = sum(abs(target - ensembleForecasted));
         err2 = abs(1-sum(weight));
-        total_err = err1+10000*err2;
+        total_err = err1+100*err2;
     end
     end_pso_main = toc(start_pso_main)
 end
