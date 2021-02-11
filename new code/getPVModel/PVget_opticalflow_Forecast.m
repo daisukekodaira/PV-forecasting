3aa82a0c76cd4bcaa63118351d073bc9fc2901c5
kazukitukazaki <67730626+kazukitukazaki@users.noreply.github.com>
2021-02-11 21:44:41 +0900

    Delete PVget_opticalflow_Forecast.m

--- new code/getPVModel/PVget_opticalflow_Forecast.m
+++
@@ -1,5 +0,0 @@
-function result_opticalflow=PVget_opticalflow_Forecast(input,~)
-    Forecastdata = input;
-    [time_steps, ~]= size(Forecastdata);
-    result_opticalflow=Forecastdata(1:time_steps,1);
-end
